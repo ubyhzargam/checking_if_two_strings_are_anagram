47fd83c924396534a1bfa0017586e7392260a365
ubyhzargam <ubyhzargam@gmail.com>
2023-06-30 16:35:46 +0530

    Update main.cpp

--- checking_if_two_strings_are_anagram/main.cpp
+++ checking_if_two_strings_are_anagram/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    int H[26]={0};
+    int i=0;
+    char ch1[50],ch2[50];
+    cout<<"Enter two lower case strings to check whether they are anagram or not : "<<endl;
+    cin>>ch1;
+    cin>>ch2;
+    for(i=0;ch1[i]!=0;i++)
+    {
+        H[ch1[i]-97]+=1;
+    }
+    for(i=0;ch2[i]!=0;i++)
+    {
+        H[ch2[i]-97]-=1;
+        if((H[ch2[i]-97])<0)
+        {
+            break;
+        }
+    }
+    if(ch2[i]=='\0'&&ch1[i]=='\0')
+        cout<<"The given strings are anagram in nature"<<endl;
+    else
+        cout<<"The given strings are not anagram in nature"<<endl;
     return 0;
 }
